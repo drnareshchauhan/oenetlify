53784c06e3a92b8973f55bd79265bf37470a9e2c
drnareshchauhan <drnareshchauhan@gmail.com>
2022-03-13 22:33:22 +0530

    netlify

---
+++ dist/precache-manifest.7560197e6c968f7d18aa87107063b52c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e2f5d9ec218fd3bb7b09f0bb8ff603",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36735df8309dbe62d8fd",
+    "url": "/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "04376733f08b93478f22",
+    "url": "/static/css/main.40afb3a9.chunk.css"
+  },
+  {
+    "revision": "36735df8309dbe62d8fd",
+    "url": "/static/js/2.0e359817.chunk.js"
+  },
+  {
+    "revision": "04376733f08b93478f22",
+    "url": "/static/js/main.5d1aec00.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4401e82d47a16230fd580bcbdb7c5d49",
+    "url": "/static/media/homepage.4401e82d.jpeg"
+  },
+  {
+    "revision": "29212f5ee6f1829c59a8b4735f2e513e",
+    "url": "/static/media/main.29212f5e.jpg"
+  }
+]);
